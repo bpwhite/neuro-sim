aa0872cfc99ee38c5be9f9540d34eb28cd94e5e0
Bryan P. White <bpcwhite@gmail.com>
2016-12-05 16:42:36 -0600

    struct sample

--- test.go
+++ test.go
@@ -2,14 +2,23 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
 func main() {
 	fmt.Println("Hello, World!")
-	var x float64
-	x = 20.0
-	fmt.Println(x)
-	fmt.Printf("x is of type %T\n", x)
 
-	//var a int = 0
+	s := person{name: "Sam", age: 50, test: 123}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+
 	for a := 0; a <= 110; a++ {
 		if a >= 100 {
 			fmt.Printf("a is: %d\n", a)
